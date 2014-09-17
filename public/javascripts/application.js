f7d2f09aac5c7a18d29fea2206a6530d8a815e48
Kirstin "Kir" Jarchow <kirstinjarchow@gmail.com>
2014-09-17 14:11:46 -0700

    mobile browser detection added

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,17 +1,24 @@
 function shakeEventDidOccur () {
   $("body").css("background-color", "red");
   setTimeout(function(){
-    $("body").css("background-color", "white");
+    $("body").css("background-color", "black");
   }, 1000);
 };
 
 window.onload = function(){
-  if (window.DeviceMotionEvent) {
-      $('#shake').html("Supported!")
+  var isMobile = jQuery.browser.mobile
+  if (window.DeviceMotionEvent && isMobile) {
+    // User device is a mobile device with the device motion event enabled
     window.addEventListener('shake', function(){
       shakeEventDidOccur();
     });
+    $(".mobile-player").show();
+  } else if (!isMobile) {
+    // User device is a computer
+    $(".computer-host").show();
   } else {
-    $('#shake').html("Not supported.")
+    // User device is a phone with the device motion event disabled
+    $(".mobile-player").show();
+    alert("Your device is not compatible. Sorry!")
   }
 }
